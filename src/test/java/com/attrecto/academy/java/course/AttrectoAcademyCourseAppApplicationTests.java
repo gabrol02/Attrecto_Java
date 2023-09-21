ce16e78e3d81a266a5561f0ed86cc8c6ae0837df
DESKTOP-KJ8CVBU\user <peter.papai@attrecto.com>
2023-09-21 10:44:56 +0200

    Első óra

---
+++ src/test/java/com/attrecto/academy/java/course/AttrectoAcademyCourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attrecto.academy.java.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttrectoAcademyCourseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
